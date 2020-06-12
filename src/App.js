a3eb7a189929e90ec7ae9b4d3e442b0f8254c65d
Aldo <miaumax@gmail.com>
2020-06-12 00:42:11 -0500

    Ultimo Commit del proyecto basico de react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Producto from './components/Producto'
+import Carrito from './components/Carrito'
 
 
 function App() {
@@ -35,6 +36,11 @@ function App() {
           agregarProducto = {agregarProducto}
         />
       ))}
+
+      <Carrito 
+          carrito = {carrito}
+          agregarProducto = {agregarProducto}
+      />
 
       <Footer 
         fecha={fecha}
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -6,17 +6,41 @@ const Producto = ({ producto, carrito, agregarProducto, productos }) => {
     // Agregando compra al carrito
     const seleccionProducto = id => {
         const producto = productos.filter(producto => producto.id === id)[0]
-        console.log(producto)
+        agregarProducto([
+            ...carrito,
+            producto
+        ])
+    }
+
+    // Eliminar productos del carrito
+    const eliminarProducto = id => {
+        const productos = carrito.filter(producto => producto.id !== id)
+
+        // Colar productos en el state
+        agregarProducto(productos)
     }
 
     return ( 
         <div>
             <h2>{nombre}</h2>
             <p>MXN {precio}</p>
-            <button
-                type = "button"
-                onClick = { () => seleccionProducto(id) } 
-            >Comprar</button>
+            
+            {productos
+            ?
+                (
+                    <button
+                        type = "button"
+                        onClick = { () => seleccionProducto(id) } 
+                    >Comprar</button>
+                )
+            :
+                    (
+                        <button
+                            type = "button"
+                            onClick = { () => eliminarProducto(id)}
+                        >Eliminar!</button>
+                    )
+            }
         </div>
     );
 }
